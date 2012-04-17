5b2f03ee8646a256f6de3f856c0f72d4441067d3
Ricardo Cerqueira <github@cerqueira.org>
2012-04-17 02:34:00 +0200

    Telephony: Add getLteOnGsmMode() method (3/3)
    Same functionality as the existing getLteOnCdmaMode, but for GSM
    LTE devices.
    
    Enable with the telephony.lteOnGsmDevice system property
    
    Change-Id: I580c37f3272c8cc8363e8db5b1c227bf6f74a04a
    Signed-off-by: Marco Zanin (B--B) <mrczn.bb@gmail.com>

--- src/com/android/phone/PhoneInterfaceManager.java
+++ src/com/android/phone/PhoneInterfaceManager.java
@@ -2382,6 +2382,10 @@ public class PhoneInterfaceManager extends ITelephony.Stub {
         }
     }
 
+    public int getLteOnGsmMode() {
+        return mPhone.getLteOnGsmMode();
+    }
+
     public void setPhone(Phone phone) {
         mPhone = phone;
     }

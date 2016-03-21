19c6871513bed3b01825a21dc2706a8ad68a51b4
Francis Guevarra <fguevarra@cyngn.com>
2016-03-21 11:43:44 -0700

    multi-sim: use getFirstPhoneForEmergencyCall()
    getFirstPhoneForEmergencyCall() checks if both sims are present
    and in service. Also, getDefaultPhone() seems to be broken on multi-sim
    devices, so use getPhoneId(0) which works on single/multi sim devices.
    
    Change-Id: Ibc90140b78f1fb23113ae5e228c9dd1338ee326b
    Issue-Id: CYNGNOS-2181

--- src/com/android/services/telephony/TelephonyConnectionService.java
+++ src/com/android/services/telephony/TelephonyConnectionService.java
@@ -629,7 +629,7 @@ public class TelephonyConnectionService extends ConnectionService {
             } catch (NullPointerException ex) {
                 Log.e(this, ex, "Exception : " + ex);
             }
-            return PhoneFactory.getDefaultPhone();
+            return getFirstPhoneForEmergencyCall();
         }
 
         int subId = PhoneUtils.getSubIdForPhoneAccountHandle(accountHandle);
@@ -671,8 +671,8 @@ public class TelephonyConnectionService extends ConnectionService {
         }
 
         if (selectPhone == null) {
-            Log.d(this, "pickBestPhoneForEmergencyCall, return default phone");
-            selectPhone = PhoneFactory.getDefaultPhone();
+            Log.d(this, "pickBestPhoneForEmergencyCall, return phone 0");
+            selectPhone = PhoneFactory.getPhone(0);
         }
 
         return selectPhone;
